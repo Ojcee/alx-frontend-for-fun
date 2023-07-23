e04b7893e957d44562ca7519f8c4bdf7bbc6848c
Ojcee <ohjacee@gmail.com>
2023-07-23 11:51:50 +0000

    markdown

---
+++ markdown2html.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+import sys
+import os
+import markdown
+
+def convert_markdown_to_html(input_file, output_file):
+    with open(input_file, 'r') as md_file:
+        markdown_text = md_file.read()
+        html_text = markdown.markdown(markdown_text)
+    
+    with open(output_file, 'w') as html_file:
+        html_file.write(html_text)
+
+if __name__ == "__main__":
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) < 3:
+        sys.stderr.write("Usage: ./markdown2html.py <input_file> <output_file>\n")
+        sys.exit(1)
+
+    input_filename = sys.argv[1]
+    output_filename = sys.argv[2]
+
+    # Check if the Markdown file exists
+    if not os.path.exists(input_filename):
+        sys.stderr.write(f"Missing {input_filename}\n")
+        sys.exit(1)
+
+    # Convert Markdown to HTML
+    convert_markdown_to_html(input_filename, output_filename)
+
+    sys.exit(0)
+
